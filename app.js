3fcaf852bea45929017d94c70ee6c7360176a9d8
samjmccormick <samjmccormick@gmail.com>
2025-01-15 10:07:47 -0800

    working authentication with hashed passwords

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /////// app.js
-
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 const path = require("node:path");
 const { Pool } = require("pg");
 const express = require("express");
@@ -8,10 +9,59 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
 const pool = new Pool({
-  // add your configuration
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+// this function isn't called directly so the 'done' function doesn't need to be defined
+// this function acts like amiddlware and will be called for us when we ask passport to authenticate
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+      const match = await bcrypt.compare(password, user.password);
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+//receives user object from sucecssful login and stores it's id property in session data
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+//upon another request with a matching session this will retrieve the id stored in session data and then queries the database for specified user
+// then done(null, user) attaches that user object to req.user so we can access it
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
 });
 
 const app = express();
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -19,6 +69,42 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", { user: req.user }));
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", async (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) {
+      alert("error in hashing");
+    } else {
+      try {
+        await pool.query(
+          "INSERT INTO users (username, password) VALUES ($1, $2)",
+          [req.body.username, hashedPassword]
+        );
+        res.redirect("/");
+      } catch (err) {
+        return next(err);
+      }
+    }
+  });
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
